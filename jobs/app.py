5d90d726572866f6788f85731162935a9830f16e
kayson <kaysonf.developer@gmail.com>
2021-03-04 12:52:54 +0800

    Completed Preparing for Dynamic Content

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
+
+
+def open_connection() -> sqlite3.Connection:
+    connection: sqlite3.Connection = getattr(g, '_connection', None)
+
+    if connection is None:
+        connection, g._connection = sqlite3.connect(PATH), sqlite3.connect(PATH)
+
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection: sqlite3.Connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection: sqlite3.Connection = open_connection()
+    cursor: sqlite3.Cursor = connection.execute(sql, values)
+
+    results = None
+    if commit:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
 
 
 @app.route('/')
